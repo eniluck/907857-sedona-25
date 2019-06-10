ed0a0ba8402c9a074b6153e219db6f0b1b1671bc
Федерягин Андрей <eniluck@mail.ru>
2019-06-10 15:37:53 +0500

    добавлена анимация shake формы

--- js/seach-form.js
+++ js/seach-form.js
@@ -1,13 +1,13 @@
 var searchFormWindow = document.querySelector('.hotel-search');
 var buttonSearch = document.querySelector('.button-search');
 
-buttonSearch.addEventListener("click", function (evt) {
+buttonSearch.addEventListener('click', function (evt) {
   evt.preventDefault();
-  searchFormWindow.classList.toggle("hidden");
-  if (searchFormWindow.classList.contains("hidden")) {
-    searchFormWindow.classList.remove("hotel-search-show");
+  searchFormWindow.classList.toggle('hidden');
+  if (searchFormWindow.classList.contains('hidden')) {
+    searchFormWindow.classList.remove('hotel-search-show');
   } else {
-    searchFormWindow.classList.add("hotel-search-show");
+    searchFormWindow.classList.add('hotel-search-show');
   }
 });
 
@@ -19,7 +19,7 @@ var adults = document.querySelector('#adults-count');
 var children = document.querySelector('#children-count'); 
 
 var isStorageSupport = true;
-var storage = "";
+var storage = '';
 var adultsCount;
 var childrenCount;
 
@@ -40,14 +40,19 @@ if (childrenCount) {
 
 arrivalDate.focus();
 
-searchForm.addEventListener("submit", function(evt) {
+searchForm.addEventListener('submit', function(evt) {
   if(!arrivalDate.value || !departureDate.value) {
     evt.preventDefault();
-    console.log("Нужно ввести дату заезда и дата выезда");
+    console.log('Нужно ввести дату заезда и дата выезда');
+    
+    searchFormWindow.classList.add('hotel-search-error');
+    setTimeout(function() {
+        searchFormWindow.classList.remove('hotel-search-error');
+    }, 600);
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("adults", adults.value);
-      localStorage.setItem("children", children.value);
+      localStorage.setItem('adults', adults.value);
+      localStorage.setItem('children', children.value);
     }
   }
 });
